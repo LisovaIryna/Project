fe9b72e1d3163dc017dcf89f8f994a7b08e49d56
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2021-05-27 15:31:24 +0300

    Добавьте файлы проекта.

---
+++ Project/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new FirstPage();
+            //MainPage = new Page_with_C_Sharp();
+            //MainPage = new NavigationPage(new FirstPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Project/FirstPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+        }
+
+        /*private void Button_Clicked(object sender, EventArgs e)
+        {
+            Label1.Text = Entry1.Text;
+        }
+
+        private async void Settings_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Setting());
+        }
+
+        private async void Play_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Game());
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }*/
+    }
+}
